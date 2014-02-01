1ed3cc5e83db60b38d0b58e937e03d797c23d921
trembizzle <tremblay.andrew@gmail.com>
2014-02-01 16:00:02 -0600

    Initial Setup
    Updated to Foundation 5

--- config.rb
+++ config.rb
@@ -5,17 +5,17 @@ add_import_path "bower_components/foundation/scss"
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "scss"
-images_dir = "images"
+images_dir = "img"
 javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
--- js/app.js
+++ js/app.js
@@ -1,3 +1,57 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+	
+	// Handle the AJAX load of the xml
+	$.get("blogger.xml", function(data){
+
+			//if XML loaded successfully find all blog entries
+			html = "";
+			
+			//get text for title and the content 
+			title = $(data).find("entry:first").find("title").text();
+			content = $(data).find("entry:first").find("content").text();
+
+			//create your own html
+			html += "<h3>" + title + "</h3>";
+			html += content;
+			html += "";
+			
+			//append html to the container of yor choice
+			$(".blog").append(html);
+		}).error(function() {
+			// Just do nothing
+	});
+	
+	// Handle the scrolling to a section
+	$(".scroll").click(function(event) {
+        // Get the full url - like mysitecom/index.htm#home
+        var full_url = $(this).attr('href');
+
+        // Split the url by # and get the anchor target name - home in mysitecom/index.htm#home
+        var parts = full_url.split("#");
+        var trgt = parts[1];
+		
+		$('html, body').animate({
+                        scrollTop: $('#'+trgt).offset().top - 100
+                    }, 1000);
+    }).error(function() {
+		// Just do nothing
+	});
+	
+	// Trackexternal links as events
+	$(".external-link").click(function(event) {
+        ga('send', 'event', 'external-link', 'click', $(this).attr('title'), $(this).attr('href'));
+    }).error(function() {
+		// Just do nothing
+	});
+	
+	// Track tab clicks as events
+	$(".tab-link").click(function(event) {
+		ga('send', 'event', 'tab', 'click', 'Skills', $(this).attr('href'));
+	}).error(function() {
+		// Just do nothing
+	});
+});
